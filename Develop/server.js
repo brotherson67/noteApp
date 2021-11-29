af21b10fbe2b966bf0d5b7bae2d839dc0d707250
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-28 21:07:20 -0700

    added listener to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,11 @@
 // imports
 const express = require('express');
 
-// instantiate express
+// instantiate express and other variables
 const app = express();
+const PORT = process.env.PORT || 3301;
+
+
+app.listen(PORT, () => {
+    console.log(`The server is now active on port ${PORT}.`)
+})
