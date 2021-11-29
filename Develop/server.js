eb6f48147cab45022fecb702a1d9e0739d06635a
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-28 21:18:31 -0700

    added server setup and started requests

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,16 @@
 // imports
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 // instantiate express and other variables
 const app = express();
+// I haven't been able to get heroku to work but this is supposed to do something with it
 const PORT = process.env.PORT || 3301;
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(express.static("public"));
 
 // Requests
 app.post('/', (req, res) => {
